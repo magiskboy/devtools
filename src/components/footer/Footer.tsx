78202ddd726eaa68bb0b33a1ed213fcac80ba00c
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2025-03-14 20:01:10 +0700

    wip
    Signed-off-by: Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="block my-6">
+      <div className="content has-text-centered">
+        Copyright &copy; {new Date().getFullYear()} - <a href="https://nkthanh.dev">nkthanh.dev</a>
+      </div>
+    </footer>
+  );
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,40 +1,19 @@
-import { useLocation, Link } from '@tanstack/react-router';
-import { useMenuContext } from '../../contexts/menu';
+import { useLocation } from '@tanstack/react-router';
 import { PropsWithChildren } from 'react';
 import cls from 'classnames';
+import { Navbar } from '../navbar';
+import { Footer } from '../footer';
 
 export const Layout: React.FC<PropsWithChildren> = ({children}) => {
-  const { title } = useMenuContext();
   const { pathname } = useLocation();
   const isHome = pathname === '/';
 
   return (
     <div className={cls({'container': true, 'py-6': isHome })}>
-      {!isHome && <Topbar title={title} />}
+      {!isHome && <Navbar />}
       {children}
       <Footer />
     </div>
   )
 }
 
-const Topbar: React.FC<{title: string}> = ({title}) => {
-  return (
-    <div className="is-flex is-align-content-center is-gap-2 mb-4">
-      <div>
-        <Link to="/"><img src="/favicon.png" width={32} height={32} /></Link>
-      </div>
-      <h4 className="is-size-5">{title}</h4>
-    </div>
-  );
-}
-
-const Footer = () => {
-  return (
-    <footer className="block my-6">
-      <div className="content has-text-centered">
-        Copyright &copy; {new Date().getFullYear()} - <a href="https://nkthanh.dev">nkthanh.dev</a>
-      </div>
-    </footer>
-
-  );
-}

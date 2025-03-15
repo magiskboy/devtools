e5f981ae7fa027a1920b912b29a303454d77ba78
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2025-03-15 11:56:26 +0700

    generate site map
    Signed-off-by: Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,27 @@ import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { analyzer } from 'vite-bundle-analyzer';
+import { generateSitemap } from 'tanstack-router-sitemap';
+import { ROUTE_CONFIG } from './src/routes.config';
+
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const siteUrl = 'https://devtools.nkthanh.dev'
+  const routes = ROUTE_CONFIG.reduce((prev, curr) => ({
+    ...prev,
+    [`${siteUrl}${curr.path}`]: {
+      changeFrequency: 'daily'
+    }
+  }), {});
+
   const config: UserConfig = {
     plugins: [
+      generateSitemap({
+        siteUrl,
+        defaultPriority: 0.5,
+        routes,
+      }),
       TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
       react(),
     ],
